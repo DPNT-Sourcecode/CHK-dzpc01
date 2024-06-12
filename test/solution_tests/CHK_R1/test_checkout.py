a4ebb91dc0ee933728444800b42dddfecbddf5a2
sbx_user1051 <sbx_user1051@169.254.104.125>
2024-06-12 18:20:26 +0000

    Wed Jun 12 18:20:26 UTC 2024

--- test/solution_tests/CHK_R1/test_checkout.py
+++ test/solution_tests/CHK_R1/test_checkout.py
@@ -2,7 +2,16 @@ from unittest import TestCase
 
 from solutions.CHL import checklite_solution
 
+
 class TestCheckout(TestCase):
     def test_incorrect(self):
-        assert checklite_solution.compute("ABCDE") == -1
+        assert checklite_solution.checklite("ABCDE") == -1
+
+    def test_discounts(self):
+        assert checklite_solution.checklite("AAAABBBCCD") == 310
+        # AAA+A = 130 + 50 = 180
+        # BB+B = 45 + 30 = 75
+        # CC = 20 * 2 = 40
+        # D = 15 * 1 = 15
+        # 180 + 75 + 40 + 15 = 310
 
